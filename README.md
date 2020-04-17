0cae6423415a0d32bcde483c389e3bed04cf035d
Hair <hair.zabala@gmail.com>
2020-04-17 21:13:44 +1000

    Primer commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Notas:
+
+Versión beta para un aplicativo de chat.
+
+Recuerden que deben de reconstruir los módulos de node con el comando
+
+```
+npm install
+```
---
+++ server/sockets/socket.js
@@ -0,0 +1,51 @@
+const { io } = require('../server');
+const { Usuarios } = require('../classes/usuarios');
+const { crearMensaje } = require('../utilidades/utilidades');
+const usuarios = new Usuarios;
+
+io.on('connection', (client) => {
+
+    client.on('entrarChat', (data, callback) => {
+
+        if (!data.nombre || !data.sala) {
+            return callback({
+                error: true,
+                mensaje: 'El nombre/sala es requerido'
+            });
+        }
+
+        client.join(data.sala);
+
+        usuarios.agregarPersona(client.id, data.nombre, data.sala);
+        client.broadcast.to(data.sala).emit('listarPersonas', usuarios.getPersonasPorSala(data.sala));
+
+        callback(usuarios.getPersonasPorSala(data.sala));
+
+    });
+
+    client.on('disconnect', () => {
+
+        let personaBorrada = usuarios.borrarPersona(client.id);
+
+        client.broadcast.to(personaBorrada.sala).emit('crearMensaje', crearMensaje('Admin', `${personaBorrada.nombre} ha abandonado el chat.`));
+        client.broadcast.to(personaBorrada.sala).emit('listarPersonas', usuarios.getPersonasPorSala(personaBorrada.sala));
+
+    });
+
+    client.on('crearMensaje', (data) => {
+
+        let persona = usuarios.getPersona(client.id);
+        let mensaje = crearMensaje(persona.nombre, data.mensaje);
+
+        client.broadcast.to(persona.sala).emit('crearMensaje', mensaje);
+
+    });
+
+    client.on('mensajePrivado', (data) => {
+        let persona = usuarios.getPersona(client.id);
+        let mensaje = crearMensaje(persona.nombre, data.mensaje);
+
+        client.broadcast.to(data.para).emit('mensajePrivado', mensaje);
+    });
+
+});
